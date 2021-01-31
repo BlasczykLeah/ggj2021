f2c686d1e236627b551ed3826ba7e205f4323b84
squidmyers33 <squidmyers33@gmail.com>
2021-01-31 10:23:18 -0600

    main menu funtionality

---
+++ doggoRescue/Assets/Scripts/Menus.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menus : MonoBehaviour
+{
+    public void play()
+    {
+        SceneManager.LoadScene(sceneBuildIndex: 1);
+    }
+    public void Exit()
+    {
+        Application.Quit();
+    }
+
+    public void returnToMenu()
+    {
+        SceneManager.LoadScene(sceneBuildIndex: 0);
+    }
+}
