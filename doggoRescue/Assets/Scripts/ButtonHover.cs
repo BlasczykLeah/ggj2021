982cb0c8eb0fa5bcd383a798397fe29d0983ebc1
Leah Blasczyk <BlasczykLeah@users.noreply.github.com>
2021-01-30 23:07:57 -0600

    playable?

---
+++ doggoRescue/Assets/Scripts/ButtonHover.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class ButtonHover : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public GameObject myImage;
+
+    void Start()
+    {
+        myImage.SetActive(false);
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        if (GetComponent<Button>().interactable) myImage.SetActive(true);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        myImage.SetActive(false);
+    }
+
+    private void OnDisable()
+    {
+        myImage.SetActive(false);
+    }
+}
